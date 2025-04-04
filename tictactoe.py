353eca92eefd2377641c21ff154925f9c435155d
1digitalnomad <41699768+1digitalnomad@users.noreply.github.com>
2025-04-03 19:03:33 -0700

    working on game to reflect if o or x won the right side of the column

---
+++ tictactoe.py
@@ -0,0 +1,16 @@
+
+def tic_tac_toe():
+    # Write a function did_I_win_2_downthat takes a player string and a two-dimensional 3 x 3 tic-tac-toe board as parameters and returns whether the player won with a column to the right.
+    # loop throught the array to see if right column
+
+
+def main():
+    # create the arrays for each line
+
+    # print array
+    # print if x won - boolean
+    # print if o won - boolean
+    pass
+
+
+main()
