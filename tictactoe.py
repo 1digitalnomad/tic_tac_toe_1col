f2f27b514117ca9868784a86a5607f9617d20b99
1digitalnomad <41699768+1digitalnomad@users.noreply.github.com>
2025-04-07 22:35:15 -0700

    finished the tic tac toe game. set this up to see if right column matched the player

--- tictactoe.py
+++ tictactoe.py
@@ -1,16 +1,26 @@
-
-def tic_tac_toe():
-    # Write a function did_I_win_2_downthat takes a player string and a two-dimensional 3 x 3 tic-tac-toe board as parameters and returns whether the player won with a column to the right.
-    # loop throught the array to see if right column
-
-
-def main():
-    # create the arrays for each line
-
-    # print array
-    # print if x won - boolean
-    # print if o won - boolean
-    pass
+def did_I_win_2_down(player, board):
+    did_win = board[0][2] == player and board[1][2] == player and board[2][2] == player
+    print(did_win)
 
 
-main()
+did_I_win_2_down('X',
+                 [
+                     ['O', 'O', 'X'],
+                     ['O', 'X', 'O'],
+                     ['X', 'O', 'O']
+                 ]
+                 )
+
+did_I_win_2_down('O', [['O', 'O', 'X'],
+                       ['O', 'X', 'O'],
+                       ['X', 'O', 'O']])
+
+
+did_I_win_2_down('X', [['O', 'O', 'X'],
+                       ['O', 'O', 'X'],
+                       ['O', 'O', 'X']])
+
+
+did_I_win_2_down('O', [['O', 'O', 'X'],
+                       ['O', 'O', 'X'],
+                       ['O', 'O', 'X']])
