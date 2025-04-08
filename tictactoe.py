385a9d16252c6a92556da080ee2ca8a989ffd8ad
1digitalnomad <41699768+1digitalnomad@users.noreply.github.com>
2025-04-07 23:11:25 -0700

    2 options

--- tictactoe.py
+++ tictactoe.py
@@ -1,26 +1,32 @@
 def did_I_win_2_down(player, board):
-    did_win = board[0][2] == player and board[1][2] == player and board[2][2] == player
-    print(did_win)
+    # first option
+    # did_win = board[0][2] == player and board[1][2] == player and board[2][2] == player
+    # print(did_win)
+    # second option
+    did_win = True
+    for x in range(3):
+        did_win &= player == board[x][2]
+    return did_win
 
 
-did_I_win_2_down('X',
-                 [
-                     ['O', 'O', 'X'],
-                     ['O', 'X', 'O'],
-                     ['X', 'O', 'O']
-                 ]
-                 )
+print(did_I_win_2_down('X',
+                       [
+                           ['O', 'O', 'X'],
+                           ['O', 'X', 'O'],
+                           ['X', 'O', 'O']
+                       ]
+                       ))
 
-did_I_win_2_down('O', [['O', 'O', 'X'],
+print(did_I_win_2_down('O', [['O', 'O', 'X'],
                        ['O', 'X', 'O'],
-                       ['X', 'O', 'O']])
+                       ['X', 'O', 'O']]))
 
 
-did_I_win_2_down('X', [['O', 'O', 'X'],
+print(did_I_win_2_down('X', [['O', 'O', 'X'],
                        ['O', 'O', 'X'],
-                       ['O', 'O', 'X']])
+                       ['O', 'O', 'X']]))
 
 
-did_I_win_2_down('O', [['O', 'O', 'X'],
+print(did_I_win_2_down('O', [['O', 'O', 'X'],
                        ['O', 'O', 'X'],
-                       ['O', 'O', 'X']])
+                       ['O', 'O', 'X']]))
